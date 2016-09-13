09bc82a293763d78fc42e33e6a152226d723d7d1
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-09-13 08:59:51 +0300

    Become 0.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-form-designer-ai',
-    version='0.11',
+    version='0.12',
     url='http://github.com/andersinno/django-form-designer-ai',
     license='BSD',
     maintainer='Anders Innovations Ltd',
