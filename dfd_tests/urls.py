03aa98d7100a86d7f57147676853789903a1a110
Aarni Koskela <akx@iki.fi>
2018-02-16 18:32:13 +0200

    dfd_tests: make urls Django 2 compatible

--- dfd_tests/urls.py
+++ dfd_tests/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import django
+
+admin_urls = (admin.site.urls if (django.VERSION[0] == 2) else include(admin.site.urls))
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin_urls),
 ]
