3172d5601d803896ef79df69a682efd9ea5c27bf
William Lindholm <wilindho@gmail.com>
2022-06-22 09:43:29 +0300

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-form-designer-ai",
-    version="1.0.1",
+    version="2.0.0",
     url="http://github.com/andersinno/django-form-designer-ai",
     license="BSD",
     maintainer="Anders Innovations Ltd",
