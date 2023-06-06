17b7b5882909793c0052c14fc5bc49711d409a49
nasereddinen <nasreddinenaseri23@gmail.com>
2023-06-06 23:47:15 +0100

    vh

--- form_designer/urls.py
+++ form_designer/urls.py
@@ -1,14 +1,6 @@
 from django.urls import re_path
-
+from django.urls import path
+from . import views 
 urlpatterns = [
-    re_path(
-        r"^(?P<object_name>[-\w]+)/$",
-        "form_designer.views.detail",
-        name="form_designer_detail",
-    ),
-    re_path(
-        r"^h/(?P<public_hash>[-\w]+)/$",
-        "form_designer.views.detail_by_hash",
-        name="form_designer_detail_by_hash",
-    ),
+   path("form_view/<int:form_definition_id>/", views._form_detail_view, name="form_designer_form_view"),
 ]
